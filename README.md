439a1ea0ef8f28e7ad47d6e3e9ce722ba12846df
Thirumal <m.thirumal@hotmail.com>
2021-05-10 09:27:01 +0530

    updated

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # asciidoc-guide
 Ascii doctor guide
+### Dependencies
+
+#### MAC
+
+   * Ruby
+
+    brew install ruby
+
+   * ASCII-Doctor
+
+    brew install asciidoctor
+
+### Linux - Ubuntu
+
+   * ASCII-Doctor
+    
+    sudo apt-get install -y asciidoctor
+
+#### Other dependencies
+
+   * For producing `mobi` from `epub`, `ebook-convert`
+     
+    brew tap homebrew/cask
+
+    brew install --cask calibre 
+    
--- make-book.sh
+++ make-book.sh
@@ -1,11 +1,11 @@
 echo "*** Producing HTML ***"
 asciidoctor tutorial.adoc
 echo "*** Producing DOCBOOK ***"
-#asciidoctor -n -b docbook -d book tutorial.adoc -o krltemp.xml 
-#sed -e s/language=\"groovy\"/language=\"java\"/ krltemp.xml > Gremlin-Graph-Guide.xml
-#rm krltemp.xml
+asciidoctor -n -b docbook -d book tutorial.adoc -o krltemp.xml 
+sed -e s/language=\"groovy\"/language=\"java\"/ krltemp.xml > tutorial.xml
+rm krltemp.xml
 echo "*** Producing EPUB ***"
-pandoc -f docbook -t epub -N --number-sections --chapters --toc --toc-depth=4 Gremlin-Graph-Guide.xml -o tutorial.epub
+pandoc -f docbook -t epub tutorial.xml -o tutorial.epub
 echo "*** Producing MOBI ***"
 ebook-convert tutorial.epub tutorial.mobi
 echo "*** Producing PDF ***"
